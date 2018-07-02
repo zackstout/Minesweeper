c9888c9e224642dec3f5948255f2d6aec9f68cc0
Zack Stout <zackstout@gmail.com>
2018-07-01 23:37:36 -0500

    numbers of adjacent bombs showing up on the buttons

---
+++ README.md
@@ -0,0 +1,2 @@
+# Minesweeper
+Building the classic game to practice designing algorithms and building user interfaces.
--- draw.js
+++ draw.js
@@ -5,20 +5,20 @@ const marginTop = 30;
 let numberButtons;
 let buttons = [];
 
+
 function setup() {
   width = 600;
   height = 600;
-  createCanvas(width, height);
-
   numberButtons = 25;
 
+  createCanvas(width, height);
   drawGrid();
   prepareBombs(20);
+  displayReality();
 }
 
 
 function handleButton() {
-  console.log(this);
   let neighbors = getNeighbors(this.x, this.y);
   console.log(neighbors);
 }
@@ -31,6 +31,7 @@ function drawGrid() {
       button.position(marginLeft + i * numberButtons * 3/4, marginTop + j * numberButtons * 3/4);
       button.x = i;
       button.y = j;
+
       // Alternative notation:
       button.location = {
         x: i,
@@ -49,11 +50,39 @@ function drawGrid() {
 // i is the x-coordinate, j the y-coordinate of the button:
 function getNeighbors(i, j) {
   let neighbors = [];
-  if (i > 0) neighbors.push({x: i - 1, y: j}); // top
-  if (j > 0) neighbors.push({x: i, y: j - 1}); // left
-  if (i < numberButtons - 1) neighbors.push({x: i + 1, y: j}); // bottom
-  if (j < numberButtons - 1) neighbors.push({x: i , y: j + 1}); // right
+  // This won't work: we need the BUTTON at the point, which is where the data is stored.
+
+  const top = getButtonAt(i - 1, j);
+  const bottom = getButtonAt(i + 1, j);
+  const left = getButtonAt(i, j - 1);
+  const right = getButtonAt(i, j + 1);
+  // if (i > 0) neighbors.push({x: i - 1, y: j}); // top
+  // if (j > 0) neighbors.push({x: i, y: j - 1}); // left
+  // if (i < numberButtons - 1) neighbors.push({x: i + 1, y: j}); // bottom
+  // if (j < numberButtons - 1) neighbors.push({x: i , y: j + 1}); // right
+
+  if (i > 0) neighbors.push(top);
+  if (j > 0) neighbors.push(left);
+  if (i < numberButtons - 1) neighbors.push(bottom);
+  if (j < numberButtons - 1) neighbors.push(right);
+
   return neighbors;
+}
+
+// throw caution to the winds:
+function getButtonAt(i, j) {
+  // buttons.forEach(btn => {
+  //   if (btn.x == i && btn.y == j) return btn;
+  //   else return 'what';
+  // });
+
+  // Yeah, foreach just can't handle returns....Weird:
+  for (let k=0; k < buttons.length; k++) {
+    const btn = buttons[k];
+    if (btn.x == i && btn.y == j) {
+      return btn;
+    }
+  }
 }
 
 
@@ -62,7 +91,7 @@ function prepareBombs(n) {
   let count = n;
   let randomIndices = [];
 
-  // Generate random indices:
+  // Generate random indices (ensuring no duplicates):
   while (count > 0) {
     let index = Math.floor(Math.random() * numberButtons * numberButtons);
     if (!randomIndices.includes(index)) {
@@ -73,6 +102,7 @@ function prepareBombs(n) {
 
   addBombs(randomIndices);
 }
+
 
 // For each index, find where it lives in the buttons array:
 function addBombs(arr) {
@@ -91,4 +121,20 @@ function addBombs(arr) {
   console.log(buttons.filter(function(x) {
      return x.bomb;
   }));
+}
+
+
+
+function displayReality() {
+  buttons.forEach(btn => {
+    let res = 0;
+    getNeighbors(btn.x, btn.y).forEach(neighbor => {
+      if (neighbor.bomb) {
+        res ++;
+      }
+    });
+
+    let p = createP(res);
+    p.position(marginLeft + btn.x * numberButtons * 3/4, marginTop + btn.y * numberButtons * 3/4);
+  });
 }
